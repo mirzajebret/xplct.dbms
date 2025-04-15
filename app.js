0daffea27b490ce36382daf8ec374a431f64d681
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2025-04-15 08:25:36 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ async function loadFiles() {
     li.innerHTML = `
       <div class="flex flex-col space-y-2 max-w-sm w-full rounded-lg mx-auto relative">
         <div class="flex flex-col items-center space-y-1">
-          <button onclick="toggleFavorit('${file.id}', ${file.favorit})" class="absolute top-2 right-2 z-10">
+          <button onclick="toggleFavorit('${file.id}', ${file.favorit})" class="absolute top-2 right-2">
             ${file.favorit ? `
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500 fill-current" viewBox="0 0 24 24"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
             ` : `
